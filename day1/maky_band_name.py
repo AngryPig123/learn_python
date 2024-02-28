10153ffee7240339cd1bd00a54876a6758d45782
Angrypig123 <shguddnr3@naver.com>
2024-02-28 18:13:29 +0900

    day1 practice

---
+++ day1/maky_band_name.py
@@ -0,0 +1,12 @@
+welcome_first = "Welcome to the Band Name Generator."
+answer_first = "What's name of the city you grew up in?"
+answer_second = "What's your pet's name?"
+result = "Your band name could be"
+
+print(welcome_first)
+print(answer_first)
+city = input()
+print(answer_second)
+pet = input()
+print(result, city, pet)
+
