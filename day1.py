568edc720ce14d8db5051e3735a9eeefd6a45627
Angrypig123 <shguddnr3@naver.com>
2024-02-28 17:58:09 +0900

    day 1 success

---
+++ day1.py
@@ -0,0 +1,45 @@
+print("Day 1 - Python Print function")
+print("The function is declared like this : ")
+print('printf("what to print")')
+
+print()
+
+print("Hello world!\nHello world!\nHello world!\nHello world!")
+print("Hello", "world!")
+print("Hello" + " world!")
+
+print()
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+print()
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+print()
+
+# print("hello " + input("what is your name?"))
+
+print()
+
+# name = input("what is your name ?")
+# print('your name length is', len(name))
+
+print()
+
+a = input()
+b = input()
+
+temp = a
+a = b
+b = temp
+
+print(a)
+print(b)
